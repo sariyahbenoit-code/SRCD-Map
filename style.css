24d4730bf5b0760efb2b9df3300e91cd5600d9e9
sariyahbenoit-code <sariyah_benoit@berkeley.edu>
2025-11-03 11:08:01 -0800

    Add files via upload

---
+++ style.css
@@ -0,0 +1,18 @@
+html, body {
+  margin: 0;
+  height: 100%;
+}
+#map {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+}
+.mapboxgl-popup {
+  max-width: 300px;
+  font-family: Arial, sans-serif;
+}
+.mapboxgl-popup img {
+  width: 100%;
+  border-radius: 4px;
+}
