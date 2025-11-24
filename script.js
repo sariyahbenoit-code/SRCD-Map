3fa922345c937163ac08e966894ab123d404de2b
sariyahbenoit-code <sariyah_benoit@berkeley.edu>
2025-11-23 16:09:50 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,78 @@
-import React, { useEffect, useRef } from 'react';
-import mapboxgl from 'mapbox-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/standard',
+    config: { basemap: { theme: 'monochrome' }},
+    zoom: 12,
+    center: [-122.51465, 37.96558],
+    pitch: 60,
+    antialias: true
+});
 
-const MapboxExample = () => {
-  const mapContainerRef = useRef(null);
-  const mapRef = useRef(null);
+fetch("data/619data.geojson")
+.then(response => response.json())
+.then(geojson => {
+    map.on("load", () => {
+        map.addSource("sites", { type: "geojson", data: geojson });
 
-  useEffect(() => {
-    mapboxgl.accessToken = 'pk.eyJ1Ijoic25iZW5vaSIsImEiOiJjbWg5Y2IweTAwbnRzMm5xMXZrNnFnbmY5In0.Lza9yPTlMhbHE5zHNRb1aA';
+        map.addLayer({
+            id: "sites-points",
+            type: "circle",
+            source: "sites",
+            paint: {
+                "circle-radius": 6,
+                "circle-color": "#ff5500"
+            }
+        });
 
-    const map = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      style: 'mapbox://styles/mapbox/standard',
-      config: { basemap: { theme: 'monochrome' }},
-      zoom: 20,
-      center: [-122.51465, 37.96558],  // your required coordinates
-      pitch: 60,
-      antialias: true
+        map.on("mouseenter", "sites-points", () => {
+            map.getCanvas().style.cursor = "pointer";
+        });
+
+        map.on("mouseleave", "sites-points", () => {
+            map.getCanvas().style.cursor = "";
+        });
+
+        map.on("click", "sites-points", e => {
+            const coords = e.features[0].geometry.coordinates.slice();
+            const props = e.features[0].properties;
+            const content = `<h3>${props.name || "Site"}</h3><p>${props.description || ""}</p>`;
+            new mapboxgl.Popup({ closeButton: true, closeOnClick: true })
+                .setLngLat(coords)
+                .setHTML(content)
+                .addTo(map);
+        });
+
+        geojson.features.forEach((feature, index) => {
+            add3DModelAtPoint(feature, index);
+        });
     });
+});
 
-    // -------------------------------------------------------
-    // Load your 619data.geojson
-    // -------------------------------------------------------
-    fetch('/data/619data.geojson')
-      .then(res => res.json())
-      .then(geojson => {
-        map.on('load
+function add3DModelAtPoint(feature, index) {
+    const lngLat = feature.geometry.coordinates;
+    const mc = mapboxgl.MercatorCoordinate.fromLngLat(lngLat, 0);
+    const rotation = [Math.PI / 2, 0, 0];
+
+    const transform = {
+        translateX: mc.x,
+        translateY: mc.y,
+        translateZ: mc.z,
+        rotateX: rotation[0],
+        rotateY: rotation[1],
+        rotateZ: rotation[2],
+        scale: mc.meterInMercatorCoordinateUnits()
+    };
+
+    const layerID = "3d-model-" + index;
+
+    const customLayer = {
+        id: layerID,
+        type: "custom",
+        renderingMode: "3d",
+        onAdd: function (map, gl) {
+            this.camera = new THREE.Camera();
+            this.scene = new THREE.Scene();
+            const light1 = new THREE.DirectionalLight(0xffffff);
+            light1.position.set(0, -70, 100).normalize();
+            this.scene.add(light1);
+            const lig
